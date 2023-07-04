2da492fff609b0e2d049ca8747797bb239ee415b
MuhammetDogukan <MuhammetDogukanBedir@gmail.com>
2023-07-04 15:44:46 +0300

    adding post, put, patc and delete endpoints

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 //builder.Services.AddScoped<IcommanderRepo, MockCommanderRepo>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IcommanderRepo, SqlCommanderRepo>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
